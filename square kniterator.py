bea00e7723ea69531d243e9ec16f9fc72e8d0cc7
wordchild <wordchild@users.noreply.github.com>
2018-04-09 13:14:25 +0100

    Code tidied up - now produces a pattern for a square in stockinette stitch only

--- square kniterator.py
+++ square kniterator.py
@@ -14,11 +14,6 @@ stitch_height = float(10 / gauge_rows)
 cast_on = int(finished_width / stitch_width) # but this should round up
 number_of_rows = int(finished_height / stitch_height)
 
-
-# print("width: ", stitch_width, " height: ", stitch_height)
-# print("cast on: ", cast_on, " knit ", number_of_rows, " number of rows")
-# print("or turned into integers ", "cast on ", int(cast_on), " knit ", int(number_of_rows), " rows")
-
 print("Pattern for a ", finished_width, " by ", finished_height, " square")
 print("CO ", cast_on, " stitches")
 print("Row 1: knit all stitches")
